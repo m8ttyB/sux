d22cf420fbf9f98d662d93beacf1c6f1ddde2dd1
Matt Brandt <mbrandt@mozilla.com>
2017-05-03 16:49:35 -0600

    added image highlighting, difference changes are highlighted the rest of the page is masked

--- highlighter/tests/test_smoke_tests.py
+++ highlighter/tests/test_smoke_tests.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 import time
+from PIL import Image, ImageChops, ImageDraw
 
 import pytest
 
@@ -10,9 +11,47 @@ class TestSmokeTests:
 
     @pytest.mark.nondestructive
     def test_can_this_work(self, base_url, selenium):
+        baseline = 'baseline.png'
         selenium.get(base_url)
         selenium.execute_script(open("./src/aria-highlighter.js").read())
         selenium.save_screenshot('screenshot.png')
         # add a sleep so the test run is visible for presentation purposes
-        time.sleep(10)
-        assert pytest.idiff('baseline.png', 'screenshot.png', tolerance=0.2)
+        time.sleep(2)
+        assert pytest.idiff(baseline, 'screenshot.png', tolerance=0.002), self.black_or_b(baseline, 'screenshot.png')
+
+    @pytest.mark.nondestructive
+    @pytest.mark.xfail(reason='Negative case: should fail, uses a tampered baseline image')
+    def test_this_should_fail(self, base_url, selenium):
+        baseline = 'baseline_tampered.png'
+        selenium.get(base_url)
+        selenium.execute_script(open("./src/aria-highlighter.js").read())
+        selenium.save_screenshot('screenshot.png')
+        assert pytest.idiff(baseline, 'screenshot.png', tolerance=0.002), self.create_image_diff_overlay(baseline, 'screenshot.png')
+
+    def create_gray_image(self, size, color):
+        img = Image.new('L', size)
+        dr = ImageDraw.Draw(img)
+        dr.rectangle((0, 0) + size, color)
+        return img
+
+    def create_image_diff_overlay(self, img_a, img_b, opacity=0.85):
+        img_a = Image.open(img_a)
+        img_b = Image.open(img_b)
+        diff = ImageChops.difference(img_a, img_b)
+        diff = diff.convert('L')
+        # Hack: there is no threshold in PILL,
+        # so we add the difference with itself to do
+        # a poor man's thresholding of the mask:
+        # (the values for equal pixels-  0 - don't add up)
+        thresholded_diff = diff
+        for repeat in range(3):
+            thresholded_diff = ImageChops.add(thresholded_diff, thresholded_diff)
+        size = diff.size
+        mask = self.create_gray_image(size, int(255 * (opacity)))
+        shade = self.create_gray_image(size, 0)
+        new = img_a.copy()
+        new.paste(shade, mask=mask)
+        new.paste(img_b, mask=thresholded_diff)
+        # Show the original image in the result, use mask=diff
+        # new.paste(img_b, mask=diff)
+        new.save('diff.png')
