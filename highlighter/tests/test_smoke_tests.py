edab57ca0c6efe87b706a24bb6c827b715b2d791
Matt Brandt <mbrandt@mozilla.com>
2017-05-17 16:09:43 -0600

    initial work - imagemagick & pdiff

--- highlighter/tests/test_smoke_tests.py
+++ highlighter/tests/test_smoke_tests.py
@@ -1,9 +1,8 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
-import time
-
 import pytest
+from pytest_selenium_pdiff import screenshot_matches
 
 
 class TestSmokeTests:
@@ -12,7 +11,11 @@ class TestSmokeTests:
     def test_can_this_work(self, base_url, selenium):
         selenium.get(base_url)
         selenium.execute_script(open("./src/aria-highlighter.js").read())
-        selenium.save_screenshot('screenshot.png')
-        # add a sleep so the test run is visible for presentation purposes
-        time.sleep(10)
-        assert pytest.idiff('baseline.png', 'screenshot.png', tolerance=0.2)
+        assert screenshot_matches(selenium, "/home/mbrandt/workspace/sux/highlighter/screenshots/test_can_this_work")
+
+    @pytest.mark.xfail(reason="This is expected to fail")
+    @pytest.mark.nondestructive
+    def test_this_should_fail(self, base_url, selenium):
+        selenium.get(base_url)
+        selenium.execute_script(open("./src/aria-highlighter.js").read())
+        assert screenshot_matches(selenium, '/home/mbrandt/workspace/sux/highlighter/screenshots/test_this_should_fail')
