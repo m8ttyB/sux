7e172def992f026330cbf2b87ce81cb68d970321
Matt Brandt <mbrandt@mozilla.com>
2017-05-17 16:25:40 -0600

    cleanup

--- highlighter/tests/test_smoke_tests.py
+++ highlighter/tests/test_smoke_tests.py
@@ -8,10 +8,10 @@ from pytest_selenium_pdiff import screenshot_matches
 class TestSmokeTests:
 
     @pytest.mark.nondestructive
-    def test_can_this_work(self, base_url, selenium):
+    def test_this_should_work(self, base_url, selenium):
         selenium.get(base_url)
         selenium.execute_script(open("./src/aria-highlighter.js").read())
-        assert screenshot_matches(selenium, "/home/mbrandt/workspace/sux/highlighter/screenshots/test_can_this_work")
+        assert screenshot_matches(selenium, "/home/mbrandt/workspace/sux/highlighter/screenshots/test_this_should_work")
 
     @pytest.mark.xfail(reason="This is expected to fail")
     @pytest.mark.nondestructive
